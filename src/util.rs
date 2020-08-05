68885c2e8de02c6cd45d002ac2d509f944ffaadc
Gijs Burghoorn <g.burghoorn@gmail.com>
2020-08-05 18:02:42 +0200

    Search function

--- src/util.rs
+++ src/util.rs
@@ -15,3 +15,17 @@ pub fn join(strings: Vec<String>, by: String) -> String {
 
     output
 }
+
+/// Function used to find a substring (spear) in a bigger string (pool)
+/// Will return None if no match is found or if the spear is empty
+pub fn search(pool: &str, spear: &str) -> Option<usize> {
+    if pool.len() < spear.len() || spear.is_empty() {
+        return None;
+    }
+
+    if pool.chars().next() == spear.chars().next() && pool[1..].starts_with(&spear[1..]) {
+        Some(0)
+    } else {
+        search(&pool[1..], spear)
+    }
+}
